ea27e9c256e2f65ec64b9e99a8a0a377b81f70c4
ricardopalhano-commits <ricardo.palhano@escola.pr.gov.br>
2026-09-10 13:45:52 -0300

    Create POO.ts

---
+++ POO.ts
@@ -0,0 +1,15 @@
+class Pessoa {
+
+    nome: string;
+    idade: number;
+
+    constructor(nome: string, idade: number) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+}
+
+const pessoa = new Pessoa("Ricardo", 15);
+console.log(pessoa.nome);
+console.log(pessoa.idade);
